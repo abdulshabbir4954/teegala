6ee59351d92c4c47c25305edbfd83818a9f55f92
abdul shabbir <abdul shabbir@Abdulshabbir>
2025-09-11 22:43:28 +0530

    Second

---
+++ teegala/src/com/tnsif/second/variables.java
@@ -0,0 +1,22 @@
+package com.tnsif.second;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 10;
+        String city = "Hyderabad";
+        double price = 99.99;
+
+        System.out.println("Number: " + number);
+        System.out.println("City: " + city);
+        System.out.println("Price: " + price);
+
+        // Changing variable values
+        number = 25;
+        price = 149.50;
+        System.out.println("Updated Number: " + number);
+        System.out.println("Updated Price: " + price);
+	}
+
+}
