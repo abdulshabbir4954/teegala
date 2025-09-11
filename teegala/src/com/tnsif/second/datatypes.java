81ca478f4110ddddb4397af6a49b55adb49da65b
abdul shabbir <abdul shabbir@Abdulshabbir>
2025-09-11 23:48:47 +0530

    second

---
+++ teegala/src/com/tnsif/second/datatypes.java
@@ -0,0 +1,20 @@
+package com.tnsif.second;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 22;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isEmployee = true;
+        String name = "suresh";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Employee: " + isEmployee);
+	}
+
+}
